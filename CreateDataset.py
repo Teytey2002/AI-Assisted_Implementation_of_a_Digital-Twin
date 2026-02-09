d9139e9c7f67a1a133fb12e2da2109056e4fd7f9
Teytey2002 <theo.desoil@ulb.be>
2026-02-09 10:27:12 +0100

    Version 1 du script qui prend les bonnes colonne et en fait un CSV quand on lui donne un fichier rpt

--- CreateDataset.py
+++ CreateDataset.py
@@ -2,8 +2,9 @@ import pandas as pd
 from pathlib import Path
 import re
 
-DATA_DIR = Path("/mnt/c/Program Files/EcosimPro/STANDARD/libs/ELECTRICAL_EXAMPLES/experiments/+filter+examples.default/exp1")
-FILE_PATH = DATA_DIR / "result_High.rpt"
+DATA_DIR = Path("/mnt/c/Program Files/EcosimPro/STANDARD/libs/ELECTRICAL_EXAMPLES/experiments/+filter+examples.default_+l+p_+sensor/exp1_+l+p_+sensor")
+FILE_PATH = DATA_DIR / "results_LP_2s.rpt"
+
 # Vérification de sécurité
 if not FILE_PATH.exists():
     raise FileNotFoundError(f"Fichier introuvable : {FILE_PATH}")
@@ -16,7 +17,7 @@ def normalize_col(c: str) -> str:
     c = re.sub(r"\([^)]*\)$", "", c)  # supprime un (...) final
     return c
 
-# 1️⃣ Trouver la ligne d'en-tête (#Group ...)
+# Trouver la ligne d'en-tête (#Group ...)
 header_idx = None
 with FILE_PATH.open("r", encoding="utf-8", errors="ignore") as f:
     for i, line in enumerate(f):
@@ -27,22 +28,16 @@ with FILE_PATH.open("r", encoding="utf-8", errors="ignore") as f:
 if header_idx is None:
     raise ValueError("Ligne d'en-tête '#Group' introuvable.")
 
-# 2️⃣ Lecture du tableau avec parsing par tabulation
+# Lecture du tableau avec parsing par tabulation
 df = pd.read_csv(FILE_PATH, skiprows=header_idx, sep="\t")
 
-# 3️⃣ Nettoyage des noms de colonnes
+# Nettoyage des noms de colonnes
 df.columns = [normalize_col(c) for c in df.columns] # Normalise les colonnes
 
 required_columns = [
     "TIME",
     "Addition_2.s_out.signal[1]",   # Vin
-    "High_freq.s_out.signal[1]",
-    "Mid_freq.s_out.signal[1]",
-    "Low_freq.s_out.signal[1]",
-    "Resistor_2.e_p.v",             # Vout_HP
-    "Capacitor_1_1.e_p.v",          # Vout_LP
-    "Capacitor_1_2.e_p.v",          # Vout_BP
-
+    "SensorVoltage_1.v"             # Vout
 ]
 
 # Vérification si les colonnes sont présentent dans le fichier
@@ -53,9 +48,9 @@ if missing_cols:
 # Création du nouveau DataFrame
 df_filtered = df[required_columns]
 
-# 6️⃣ Sauvegarde dans un nouveau fichier
+# Sauvegarde dans un nouveau fichier
 df_filtered.to_csv(
-    "result_High_filtered.csv",
+    "results_LP_2s.csv",
     index=False
 )
 
