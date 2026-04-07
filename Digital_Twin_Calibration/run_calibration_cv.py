3d186db67d83c599f1882cd28abb951fe6754c63
Sam <samuel.alves-nunes@cegelec.com>
2026-04-07 14:47:27 +0200

    delete NN calibration beceause now, do it on inference Add new result for Irace in calibrator and not just an hyperparameter tunning

--- Digital_Twin_Calibration/run_calibration_cv.py
+++ Digital_Twin_Calibration/run_calibration_cv.py
@@ -77,13 +77,6 @@ def main() -> None:
             f"rmse={fold.test_metrics.rmse:.6g} nmse={fold.test_metrics.nmse:.6g}"
         )
 
-    # Version to use NN, but maybe, no  needs LeaveOneExperimentOutCV
-    # Mayby add argument to run with good simulation and calibrator by using arg in commadn line
-    #calibrator = RCNeuralCalibrator.load("./src/dtcalib/deep_learning/models/rc_inverse_best_2026-03-09_10-11-18.pth")
-    #report = calibrator.calibrate(ds.experiments)
-    #print("NN Global C_hat:", report.theta_hat)
-    #print("Variance:", report.cost)
-
 
 if __name__ == "__main__":
     main()
